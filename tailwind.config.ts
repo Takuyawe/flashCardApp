3f3fdfac0ff312e69acb1e3e630b370e15dd3902
Takuya Nemoto <takuslugger200@gmail.com>
2024-04-10 12:54:51 +0900

    save

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'base-dark': '#050023',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;

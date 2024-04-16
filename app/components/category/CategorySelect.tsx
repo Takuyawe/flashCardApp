01107f9538cd4bd5ea94a5bc757d626cb1490281
Takuya <takuslugger200@gmail.com>
2024-04-16 15:43:44 +0900

    refactor: move category item component

--- app/components/category/CategorySelect.tsx
+++ app/components/category/CategorySelect.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { CategoryItem } from './CategoryItem';
 
 const CATEGORIES: Record<
   string,
@@ -70,57 +71,6 @@ const CATEGORIES: Record<
   16: {
     name: 'Snakes',
   },
-};
-
-type ItemProps = {
-  category: {
-    name: string;
-    children?: Record<
-      number,
-      { name: string; children?: Record<string, { name: string }> }
-    >;
-  };
-  chosenCategory: string;
-  setCategory: React.Dispatch<React.SetStateAction<string>>;
-};
-
-const CategoryItem = ({ category, chosenCategory, setCategory }: ItemProps) => {
-  const [isChildrenOpen, setIsChildrenOpen] = useState(true);
-
-  return (
-    <div className="flex flex-col">
-      <div className="flex items-center gap-x-1">
-        <button
-          onClick={() => setCategory(category.name)}
-          className="flex items-center justify-center h-4 w-4 rounded-full bg-white outline outline-base-dark">
-          {chosenCategory === category.name && (
-            <i className="ri-circle-fill text-base-dark text-md" />
-          )}
-        </button>
-        {category.children && !isChildrenOpen ? (
-          <i className="ri-arrow-right-s-line text-xl" />
-        ) : (
-          <i className="ri-arrow-down-s-line text-xl" />
-        )}
-        <button onClick={() => setIsChildrenOpen(!isChildrenOpen)}>
-          <i className="ri-folder-fill text-bright-blue text-2xl" />
-        </button>
-        <span>{category.name}</span>
-      </div>
-      {isChildrenOpen && category.children && (
-        <div className="ml-4">
-          {Object.keys(category.children).map((childKey) => (
-            <CategoryItem
-              key={childKey}
-              category={category.children![parseInt(childKey)]}
-              chosenCategory={chosenCategory}
-              setCategory={setCategory}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
 };
 
 type Props = {

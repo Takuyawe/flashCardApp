5dee88739b527f46794966a614d554eabcc95fc3
Takuya <takuslugger200@gmail.com>
2024-04-23 15:32:28 +0900

    change folder name

--- oldCode/login.tsx
+++ oldCode/login.tsx
@@ -4,7 +4,7 @@ import { Auth } from '@supabase/auth-ui-react';
 import { FaFacebook } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 import { authenticator } from '~/server/auth.server';
-import { SocialAccountButton } from '~/components/login/SocialAccountButton';
+import { SocialAccountButton } from '~/components/auth/SocialAccountButton';
 import { convertTextToProvider } from '~/modules/auth/convertTextToProvider';
 import { createSupabaseServerClient } from '~/server/supabase.server';
 import { AUTHENTICATOR_STRATEGY_NAME } from '~/constants/Authentication';

bb1f37506acd62574c2edec0838ab5c674c5fe89
Takuya <takuslugger200@gmail.com>
2024-04-30 18:59:21 +0900

    save

--- app/components/browse/SearchBar.tsx
+++ app/components/browse/SearchBar.tsx
@@ -1,12 +1,17 @@
+import { SearchResults } from './SearchResults';
+
 export const SearchBar = () => {
   return (
     <div className="relative">
-      <i className="ri-search-line text-xl absolute left-2 top-1 opacity-75" />
-      <input
-        name="search"
-        placeholder="Search a word"
-        className="h-9 w-72 border-2 border-base-dark rounded-md pl-8 text-md"
-      />
+      <div className="relative">
+        <i className="ri-search-line text-xl absolute left-2 top-1 opacity-75" />
+        <input
+          name="search"
+          placeholder="Search a word"
+          className="h-9 w-72 border-2 border-base-dark rounded-md pl-8 text-md"
+        />
+      </div>
+      <SearchResults />
     </div>
   );
 };
---
+++ app/components/browse/SearchResults.tsx
@@ -0,0 +1,7 @@
+export const SearchResults = () => {
+  return (
+    <div className="absolute top-10 min-h-48 h-auto w-72 bg-white border border-base-dark rounded-md">
+      SearchResults
+    </div>
+  );
+};

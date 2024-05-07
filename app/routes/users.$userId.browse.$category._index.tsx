695063067d44bd8ad1b9b31dfdecdd769a11eaa4
Takuya <takuslugger200@gmail.com>
2024-05-07 10:24:31 +0900

    refactor

--- app/routes/users.$userId.browse.$category._index.tsx
+++ app/routes/users.$userId.browse.$category._index.tsx
@@ -1,13 +1,13 @@
-import { json, LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { useMemo } from "react";
-import { useRecoilState } from "recoil";
-import { categoriesAtom } from "~/atoms/atom";
-import { CategoriesList } from "~/components/browse/CategoriesList";
+import { json, LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { useMemo } from 'react';
+import { useRecoilState } from 'recoil';
+import { categoriesAtom } from '~/atoms/atom';
+import { CategoriesList } from '~/components/browse/CategoriesList';
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
-  const categoryId = url.searchParams.get("categoryId");
+  const categoryId = url.searchParams.get('categoryId');
 
   return json({ categoryId });
 };
@@ -22,7 +22,7 @@ export default function Index() {
   }, [categories, categoryId]);
 
   return (
-    <div className="flex-1">
+    <div className="flex-1 overflow-y-auto">
       <CategoriesList
         categories={categories}
         parentCategoryId={parentCategoryId as string}

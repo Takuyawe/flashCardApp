dac8ffbfbd148fba2c36ef238a5e85fd423da2d3
Takuya <takuslugger200@gmail.com>
2024-05-07 15:21:07 +0900

    feat: add shadcn

--- app/constants/Quiz/index.ts
+++ app/constants/Quiz/index.ts
@@ -1,5 +1,13 @@
 export const TOTAL_QUESTIONS = 10;
+
+// Gauge
 export const GAUGE_CX = 60;
 export const GAUGE_CY = 60;
 export const GAUGE_RADIUS = 50;
 export const GAUGE_CIRCUMFERENCE = 2 * Math.PI * 50;
+
+// Comments after answering
+export const GOOD_LUCK_NEXT_TIME = 'Good luck next time!';
+export const GOOD_JOB = 'Good job! Keep it up!';
+export const FULL_SCORE =
+  "Congratulations! You've answered all questions correctly!";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,60 +1,80 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss"
 
-export default {
-  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      // TODO: settings for breakpoints
-      height: {
-        body: 'calc(100dvh - 6rem)',
-      },
       colors: {
-        'bright-purple': '#F14DFF',
-        'bright-green': '#4F8A10',
-        'bright-red': '#cc0000',
-        'base-dark': '#050023',
-        'light-dark': '#35343D',
-        'smoke-light': 'rgba(0, 0, 0, 0.5)',
-        'bright-blue': '#78C6FF',
-        'light-blue': '#90EBFF',
-        facebook: '#2c64f6',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      animation: {
-        'fade-in':
-          'fade-in 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both',
-        'fade-out': 'fade-out 0.5s linear   both',
-        'slide-in-top':
-          'slide-in-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both',
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        'fade-in': {
-          '0%': {
-            opacity: '0',
-          },
-          to: {
-            opacity: '1',
-          },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'fade-out': {
-          '0%': {
-            opacity: '1',
-          },
-          to: {
-            opacity: '0',
-          },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
-        'slide-in-top': {
-          '0%': {
-            transform: 'translateY(-1000px)',
-            opacity: '0',
-          },
-          to: {
-            transform: 'translateY(0)',
-            opacity: '1',
-          },
-        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config

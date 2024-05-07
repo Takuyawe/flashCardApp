21bdda2af8ce7d0804079bff104b1b8ff9f37bcc
Takuya <takuslugger200@gmail.com>
2024-05-07 19:02:28 +0900

    save

--- app/routes/users.$userId.quiz.new.$category.tsx
+++ app/routes/users.$userId.quiz.new.$category.tsx
@@ -1,16 +1,43 @@
+import Anthropic from '@anthropic-ai/sdk';
 import { json, LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { QUIZ_INSTRUCTION } from '~/constants/AIInstruction';
+import { generateQuizWords } from '~/modules/quiz/generateQuizWords';
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const quizLevel = url.searchParams.get('quizLevel');
   const category = params.category;
 
-  console.log(quizLevel);
-  console.log(category);
+  if (!quizLevel || !category) {
+    return json({
+      error: 'Something went wrong. Please try again.',
+    });
+  }
 
-  return json({ quizLevel });
+  const apiKey = process.env.CLAUDE_API;
+  const anthropic = new Anthropic({
+    apiKey: apiKey,
+  });
+
+  const instruction =
+    quizLevel === 'easy'
+      ? QUIZ_INSTRUCTION.EASY
+      : QUIZ_INSTRUCTION.NORMAL_AND_HARD;
+
+  const quizWords = await generateQuizWords(
+    anthropic,
+    instruction,
+    category as string
+  );
+
+  return json({ quizWords });
 };
 
 export default function Layout() {
+  const loaderData = useLoaderData<typeof loader>();
+
+  console.log(loaderData);
+
   return '';
 }
